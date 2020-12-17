f8257f47a696fa62777471b55a5d7c8a93860ec6
0gnev <j.kodanavi@gmail.com>
2020-12-17 20:43:47 +0700

    fix paths

--- conf/config.php
+++ conf/config.php
@@ -6,8 +6,8 @@ define("MODEL_PATH", ROOT. "/models/");
 define("VIEW_PATH", ROOT. "/views/");
 
 
-require_once("Entry.php ");
-require_once("route.php");
-require_once MODEL_PATH. 'Model.php';
-require_once VIEW_PATH. 'View.php';
-require_once CONTROLLER_PATH. 'Controller.php';
+require_once("../Entry.php");
+require_once("../Route.php");
+require_once MODEL_PATH. '../Model.php';
+require_once VIEW_PATH. '../View.php';
+require_once CONTROLLER_PATH. '../Controller.php';
--- public/index.php
+++ public/index.php
@@ -1,4 +1,4 @@
 <?php
-require_once("conf/config.php");
+require_once("../conf/config.php");
 
 Routing::buildRoute();
